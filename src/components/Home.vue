cf24f5d0349e2d83417f3b06f64d5f13ef7b41fc
bassirou <ndiayebasse@gmail.com>
2018-10-31 16:16:01 +0100

    add commponents

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,59 @@
 <template>
     <div class="home_container theme_section">
-        Home Section
+        <div class="header_title">
+            <h2>{{mainTitle}}</h2>
+            <p>{{headerText}}</p>
+        </div>
+
+        <b-container>
+            <b-row>
+                <b-col class="col-sm" v-for="item in servicesItems" :key="item.title">
+                    <div class="srItem">
+                        <div class="image">
+                            <img :src="item.iconService" height="160px" />
+                        </div>
+                        <h3>{{item.title}}</h3>
+                        <p>{{item.description}}</p>
+                    </div>
+                </b-col>
+
+            </b-row>
+        </b-container>
     </div>
+
 </template>
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      mainTitle: "Services",
+      headerText: "All their equipment and instruments are alive.",
+      servicesItems: [
+        {
+          title: "Almost before we knew it, we had left the ground",
+          iconService:
+            "https://image.freepik.com/vecteurs-libre/fond-d-39-ecran-religieux-eid-mubarak_1055-2500.jpg",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud."
+        },
+        {
+          title: "A shining crescent far beneath the flying vessel",
+          iconService:
+            "https://image.freepik.com/vecteurs-libre/fond-d-39-ecran-religieux-eid-mubarak_1055-2500.jpg",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud."
+        },
+        {
+          title: "She stared through the window at the stars",
+          iconService:
+            "https://image.freepik.com/vecteurs-libre/fond-d-39-ecran-religieux-eid-mubarak_1055-2500.jpg",
+          description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud."
+        }
+      ]
+    };
+  }
+};
 </script>
 <style lang="scss">
 </style>
